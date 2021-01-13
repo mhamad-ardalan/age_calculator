476a15de22eb0f3859b2e862c39a1fea3599d10a
mhamad-ardalan <77403740+mhamad-ardalan@users.noreply.github.com>
2021-01-13 20:24:08 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace age_calculator
         static void Main(string[] args)
         {
        Console.WriteLine(" type your age ? ");
-int born_year = Console.ReadLine();
+int input = Console.ReadLine();
+int born_year = Int32.Parse(input);
 int this_year = 2021;
 int you_age =  this_year - born_year;
 Console.WriteLine($" your born year {you_age} ");
